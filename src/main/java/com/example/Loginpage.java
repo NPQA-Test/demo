224b40662d63cb76c29146032bf6f20289085830
imac <naimish.patoliya@testgrid.io>
2024-11-20 21:27:01 +0530

    commit message test loginpage

--- src/main/java/com/example/Loginpage.java
+++ src/main/java/com/example/Loginpage.java
@@ -1,12 +1,30 @@
 package com.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Loginpage {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) throws InterruptedException {
+        // Add Server Link Here
+        String Websitelink = "https://Amazon.in/";
 
-    System.out.println("Push To github This Test Case.");
-    System.out.println( "Hello World!" );
+        WebDriver driver = null;
 
+        System.setProperty("webdriver.chrome.driver", "/Users/imac/Documents/LocalBrowser/chromedriver-mac-x64/chromedriver");
+        driver = new ChromeDriver();
 
+        // Open the login page URL
+        driver.get(Websitelink);
+        driver.manage().window().maximize();
+
+        loginPage(driver);
+
+        driver.quit();
+        Thread.sleep(5000);
+    }
+
+    private static void loginPage(WebDriver driver) throws InterruptedException {
+
+        System.out.println( "Hello World!" );
     }
 }

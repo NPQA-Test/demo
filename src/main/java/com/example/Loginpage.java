63f1760ed04ed5b1b24ccb5dc85600de568d6855
imac <naimish.patoliya@testgrid.io>
2024-11-20 19:29:17 +0530

    commit message test loginpage

---
+++ src/main/java/com/example/Loginpage.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Loginpage {
+    public static void main( String[] args )
+    {
+
+    System.out.println("Push To github This Test Case.");
+    System.out.println( "Hello World!" );
+
+
+    }
+}

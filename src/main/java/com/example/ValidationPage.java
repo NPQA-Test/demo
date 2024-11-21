161ceb7554db814d50b323ff8cbbe1cc6b25aa31
imac <naimish.patoliya@testgrid.io>
2024-11-21 17:39:13 +0530

    In sub branch

---
+++ src/main/java/com/example/ValidationPage.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ValidationPage {
+     public static void main(String[] args) throws InterruptedException {
+        // Add Server Link Here
+        String Websitelink = "https://Amazon.in/";
+
+        WebDriver driver = null;
+
+        System.setProperty("webdriver.chrome.driver", "/Users/imac/Documents/LocalBrowser/chromedriver-mac-x64/chromedriver");
+        driver = new ChromeDriver();
+
+        // Open the login page URL
+        driver.get(Websitelink);
+        System.out.println("Driver Created");
+        driver.manage().window().maximize();
+
+        loginPage(driver);
+
+        driver.quit();
+        Thread.sleep(5000);
+    }
+
+    private static void loginPage(WebDriver driver) throws InterruptedException {
+
+        System.out.println( "Validation Of Login Page Start :" );
+    }
+}
